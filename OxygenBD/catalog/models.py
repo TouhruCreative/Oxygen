055e8ab03e452c8d72a150543676f90cc2060bb9
TouhruCreative <touhru.shott@gmail.com>
2026-04-02 21:51:12 +0500

    Start add category model

--- OxygenBD/catalog/models.py
+++ OxygenBD/catalog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+"""
+Model architecture
+
+Category
+ ├── name (CharField)
+ ├── parent (FK → Category, null=True)
+"""
+# TODO complete parent
+class Shop(models.Model):
+
+    name = models.CharField()
+    parent = None
+    
+    class Meta:
+        verbose_name = _("Category")
+        verbose_name_plural = _("c")
+
+    def str(self):
+        return self.name
