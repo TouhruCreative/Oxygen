7b652bcd2df70cedea0869a327ad1e28863c4c8b
TouhruCreative <touhru.shott@gmail.com>
2026-04-02 22:12:05 +0500

    add new models to cart: cart and cart item

--- OxygenBD/cart/models.py
+++ OxygenBD/cart/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from users.models import User
+from catalog.models import ProductVariant
+"""
+Model architecture
 
-# Create your models here.
+Cart
+ ├── user (OneToOne → User)
+ ├── created_at (DateTimeField)
+
+ CartItem
+ ├── cart (FK → Cart)
+ ├── product_variant (FK → ProductVariant)
+ ├── quantity (IntegerField)
+
+"""
+
+class Cart(models.Model):
+    user = models.OneToOneField(User, verbose_name=_(""), on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=True, auto_now_add=True)
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, verbose_name=_(""), on_delete=models.CASCADE)
+    product_variant = models.ForeignKey(ProductVariant, verbose_name=_(""), on_delete=models.CASCADE)
+    quantity = models.IntegerField()
