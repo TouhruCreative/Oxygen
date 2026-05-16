3b3d53b8ec461a9b920437423c73239582a9f902
TouhruCreative <touhru.shott@gmail.com>
2026-05-16 10:32:33 +0500

    Add to DB - price of product and update html templates, when uses price

--- OxygenBD/catalog/models.py
+++ OxygenBD/catalog/models.py
@@ -27,7 +27,7 @@ class Product(models.Model):
     name = models.CharField(max_length=150)
     description = models.TextField(blank=True)
     is_active = models.BooleanField(default=True)
-
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
